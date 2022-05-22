130c12d8c06fdc7a4220c01f9f171bd2938e3ab6
moustf <tepro.18@gmail.com>
2022-05-23 00:15:00 +0300

    Added the function of the error and the event listeners to the elements close #8

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,5 +1,4 @@
 // ? Starting the targeting of the elements.
-
 const billInput = document.getElementById("bill");
 const customInput = document.getElementById("custom");
 const peopleInput = document.getElementById("ppl");
@@ -11,3 +10,24 @@ const fiftyButton = document.querySelector(".fifty-ratio");
 const tipResult = document.querySelector(".tip-result");
 const totalResult = document.querySelector(".total-result");
 const resetButton = document.querySelector(".reset");
+
+// ? Adding the event listener to the bill input field.
+billInput.onblur = function () {
+  if (billInput.value === "0") {
+    zeroError(billInput);
+  }
+};
+
+// ? Adding the event listener to the number of people input field.
+peopleInput.onblur = function () {
+  if (peopleInput.value === "0") {
+    zeroError(peopleInput);
+  }
+};
+
+// ? Creating the function which is responsible for showing the error text and making the outline red.
+function zeroError(ele) {
+  ele.style.outline = "3px solid red";
+  const parent = ele.parentElement;
+  parent.querySelector(".error").style.display = "block";
+}
