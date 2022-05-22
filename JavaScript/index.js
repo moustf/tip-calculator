5b11c578154e63832dd1e6be2c5352195e18ef0f
moustf <tepro.18@gmail.com>
2022-05-23 00:12:03 +0300

    Targeted the elements needed and the input fields close #8

---
+++ JavaScript/index.js
@@ -0,0 +1,13 @@
+// ? Starting the targeting of the elements.
+
+const billInput = document.getElementById("bill");
+const customInput = document.getElementById("custom");
+const peopleInput = document.getElementById("ppl");
+const fiveButton = document.querySelector(".five-ratio");
+const tenButton = document.querySelector(".ten-ratio");
+const fifteenButton = document.querySelector(".fifteen-ratio");
+const twentyFiveButton = document.querySelector(".twenty-five-ratio");
+const fiftyButton = document.querySelector(".fifty-ratio");
+const tipResult = document.querySelector(".tip-result");
+const totalResult = document.querySelector(".total-result");
+const resetButton = document.querySelector(".reset");
